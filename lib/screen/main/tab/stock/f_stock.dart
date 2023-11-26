a3beac47f7f35e56e91ca119cef5d19e724ba001
Eunseo-415 <eumseo@icloud.com>
2023-11-27 07:22:43 +0900

    feat: stock 주식 탭-이미지버튼

--- lib/screen/main/tab/stock/f_stock.dart
+++ lib/screen/main/tab/stock/f_stock.dart
@@ -1,4 +1,7 @@
+import 'package:fast_app_base/common/common.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../common/widget/w_image_button.dart';
 
 class StockFragment extends StatefulWidget {
   const StockFragment({super.key});
@@ -8,8 +11,76 @@ class StockFragment extends StatefulWidget {
 }
 
 class _StockFragmentState extends State<StockFragment> {
+  final tabController = TabController(length: length, vsync: vsync)
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Colors.black,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            actions: [
+              ImageButton(
+                imagePath: "$basePath/icon/stock_search.png",
+                onTap: () {
+                  context.showSnackbar("검색");
+                },
+              ),
+              ImageButton(
+                imagePath: "$basePath/icon/stock_calendar.png",
+                onTap: () {
+                  context.showSnackbar("캘린더");
+                },
+              ),
+              ImageButton(
+                imagePath: "$basePath/icon/stock_settings.png",
+                onTap: () {
+                  context.showSnackbar("설정");
+                },
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [title, tabBar, myAccount, height20, myStocks],
+            ),
+          )
+        ],
+      ),
+    );
   }
+
+  Widget get title => Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          "토스증권".text.size(24).bold.make(),
+          width20,
+          'S&P 500'
+              .text
+              .size(13)
+              .bold
+              .color(context.appColors.lessImportantText)
+              .make(),
+          width10,
+          3919.29
+              .toComma()
+              .text
+              .size(13)
+              .bold
+              .color(context.appColors.plus)
+              .make()
+        ],
+      ).pOnly(left: 20);
+
+  Widget get tabBar => Column(children: [
+        TabBar(
+          tabs: ["내 주식".text.make(), "오늘의 발견".text.make()],
+        ),
+        Line()
+      ]);
+
+  Widget get myAccount => Placeholder();
+
+  Widget get myStocks => Placeholder();
 }
